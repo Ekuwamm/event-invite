bb9d95872801f5a64dd6dc1468fe333529e9fe0a
Moses Ekuwam <mosekuwam@yahoo.com>
2025-01-06 00:11:16 +0000

    fifth commit

--- events/forms.py
+++ events/forms.py
@@ -4,4 +4,4 @@ from .import models
 class CreateEvent(forms.ModelForm):
     class Meta:
         model = models.myevent
-        fields = ['title','body1','body1','body2','body3','body4','slug','banner']
+        fields = ['title','body1','body1','body2','slug','banner']
